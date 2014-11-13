49e85ae8ae0dcb861717f98d17fd899207c1a07d
kublahanov <4progs@inbox.ru>
2014-11-13 17:12:34 +0300

    adding docs index, fixing log problems

--- web/index.php
+++ web/index.php
@@ -11,16 +11,16 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => arraY(__DIR__ . '/../views')
 ));
 
+// Функция get_ip() определена в библиотеке super_lib.
+// Благодаря сгенерированному autoloader.php нужный файл библиотеки подключится автоматически
+$ip = get_ip();
+
 // При заходе в корень нашего сайта, сработает контроллер описанный в анонимной функции ниже
-$app->get('/', function() use ($app) {
+$app->get('/', function() use ($app, $ip) {
 
     // TimerClass определён в библиотеке super_lib.
     // Благодаря сгенерированному autoloader.php нужный файл библиотеки подключится автоматически
     $timer = new TimerClass();
-
-    // Функция get_ip() определена в библиотеке super_lib.
-    // Благодаря сгенерированному autoloader.php нужный файл библиотеки подключится автоматически
-    $ip = get_ip();
 
     $templateVars = array(
         'msg' => 'Super Hello World',
@@ -33,12 +33,12 @@ $app->get('/', function() use ($app) {
 
 });
 
-$app->finish(function() {
+$app->finish(function() use ($ip) {
 
     // Класс MyCompanyNamespace\SuperLogger определён в Composer-пакете mycompany/superlogger
     // Благодаря сгенерированному autoloader.php нужный файл с описанием класса подключится автоматически
     $logger = new MyCompanyNamespace\SuperLogger();
-    $logger->writeLog('log.txt', 'Someone visited the page');
+    $logger->writeLog($_SERVER['DOCUMENT_ROOT'] . '/logs/log.txt', 'Someone visited the page: ' . $ip);
 
 });
 
